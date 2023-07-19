d2abb8266f6126a108c1ddc36c674cded8e4fd12
Sagar7421 <imsagar2922@gmail.com>
2023-07-19 10:11:01 -0400

    Implemented company profile saving during signup #6_2 #55

--- Sprint 2/backend/controller/signUpController.js
+++ Sprint 2/backend/controller/signUpController.js
@@ -38,3 +38,59 @@ const candidateSignup = async (req, res) => {
       return res.status(500).json({ message: 'Internal server error' });
     }
   };
+
+  const createUser = async (req, res) => {
+
+
+    try {
+        const { email } = req.body;
+        const userExist = await User.findOne({ email })
+        if (userExist) {
+            return res.status(400).json({
+                success: false,
+                message: "E-mail already exists",
+                value: userExist,
+                Received: req.body
+            });
+        }
+
+        if (req.body.company) {
+            const { company } = req.body;
+            const companyExist = await User.findOne({ company })
+            if (companyExist) {
+                return res.status(400).json({
+                    success: false,
+                    message: "company already exists",
+                    value: { company },
+                    Received: req.body
+                })
+            }
+        }
+
+        const userDetails = req.body;
+
+        const collectionSize = (await User.collection.stats()).size.toString()
+
+        if (userDetails.User_type == "Company") {
+            userDetails._id = userDetails.company + collectionSize;
+        }
+        else {
+            userDetails._id = userDetails.name + "-" + userDetails.lastname + collectionSize
+        }
+
+        const newUser = await User.create(userDetails)
+        
+        await newUser.save();
+
+        res.status(200).json({
+            success: true,
+            status:'success',
+            value:newUser,
+        })
+
+    } catch (error) {
+        console.error('Error during signup:', error);
+        return res.status(500).json({ success: false, message: 'Internal server error', status: 'fail' });
+    }
+
+};
