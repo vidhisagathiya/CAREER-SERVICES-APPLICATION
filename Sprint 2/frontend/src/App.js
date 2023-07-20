5feeee3362d0b07c4f7dd23f4c5d6000ffff8f65
Karnik Shah <shahkarnik@gmail.com>
2023-07-20 13:02:47 -0400

    Updated App.js for all routers

--- Sprint 2/frontend/src/App.js
+++ Sprint 2/frontend/src/App.js
@@ -1,14 +1,32 @@
 import './css_files/App.css';
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useState } from 'react';
+import Home from './components/pages/Home';
+import EditProfile from './components/pages/EditProfile';
+import Navbar from './components/fixed/Navbar'
+import Profile from './components/pages/Profile';
+import SignIn from './components/pages/SignIn';
 import SignUp from './components/pages/SignUp';
-
+import Post from './components/pages/Post';
+import UserContext from "./UserContext";
+import Explore_Jobs from './components/pages/Explore_Jobs';
+import Individual_Post from './components/pages/Individual_Post';
+import ManagePosts from './components/pages/CompanyPosts';
+import PostDetails from './components/pages/PostDetails';
+import Interviews from './components/pages/Interviews';
+import  ThemeProvider  from './components/theme/ThemeProvider';
+import AllApplications from './components/pages/AllApplications';
 
 function App () {
-
+  const [user, setUser] = useState(() => {
+    const storedUser = localStorage.getItem("user");
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   return (
-
-   
+    <UserContext.Provider value={{ user, setUser }}>
+      <ThemeProvider>
+        <Navbar/>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Navigate to="/home" />} />
@@ -26,7 +44,8 @@ function App () {
                 <Route path="/applications" element={<AllApplications/>}/>
             </Routes>
             </BrowserRouter>
-       
+            </ThemeProvider>
+       </UserContext.Provider>
     );
 }
 
